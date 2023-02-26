fe6cabf083dc39408744c3d52cee957f1d40c284
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-02-26 18:18:41 +0200

    feat:user signin

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return { msg: 'Signed in' };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash; // deletes the hash atribute from user.
+
+      return user;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('Credentials Taken');
+      }
+      throw error;
+    }
   }
 
-  signup() {
-    return { msg: 'Signed up' };
+  login(dto: AuthDTO) {
+    return { msg: 'Signed in' };
   }
 }
